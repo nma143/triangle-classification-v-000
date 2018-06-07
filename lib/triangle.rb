f1846d5903377954d3e419829a9d0241970d1fad
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-06-07 23:32:22 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,7 +6,7 @@ class Triangle
   def initialize(length1, length2, length3)
     @length1 = length1
     @length2 = length2
-    @length3 = length3    
+    @length3 = length3
   end
 
 
@@ -14,10 +14,10 @@ class Triangle
 
     if @length1==@length2 && @length1==@length3
       return :equilateral
-    elsif @length1==@length2 | @length1==@length3
+    elsif @length1==@length2 || @length1==@length3
       return :isosceles
     else
-      return :scalene    
+      return :scalene
     end
 
   end
