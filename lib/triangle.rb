68e2e525ad1c9ac892422b9c8816adff68dcb181
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-06-07 23:39:20 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,7 +17,7 @@ class Triangle
     elsif @length1==@length2 || @length1==@length3 || @length2==@length3
       return :isosceles
     elsif @length1 <= 0 || @length2 <= 0 || @length3 <= 0
-
+      raise TriangleError  
     elsif @length1 + @length2 >= @length3 || @length1 + @length3 >= @length2 || @length2 + @length3 >= @length1
         return :scalene
     else
